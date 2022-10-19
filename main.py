0331ea0ad4ff0c954b2197e3332e2a2d309c65e7
dd <vicdev9@gmail.com>
2022-10-19 16:14:51 +0200

    main.py on port 8080

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ class App:
 app = App()
 
 if __name__ == "__main__":
-    uvicorn.run("backend_sim:app", host="127.0.0.1", port=80, log_level="info")
+    uvicorn.run("backend_sim:app", host="127.0.0.1", port=8080, log_level="info")
